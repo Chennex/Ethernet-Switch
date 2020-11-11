e03cefe28aa5f4b3f28be0ff55611787d26a59d2
Chennex <jhnklpstrp@gmail.com>
2020-11-11 15:14:20 +0100

    Added FCS components
    FCS components and port mapping added.

--- Switch/FCS/FCSBlockTop.vhd
+++ Switch/FCS/FCSBlockTop.vhd
@@ -13,7 +13,7 @@ inputC : 	in std_logic_vector(7 downto 0);
 inputD : 	in std_logic_vector(7 downto 0);
 
 --MAC Learning connections
-Wport : 	in std_logic_vector(3 downto 0);
+WportMAC : 	in std_logic_vector(3 downto 0);
 src : 		out std_logic_vector(47 downto 0);
 dst : 		out std_logic_vector(47 downto 0);
 
@@ -22,17 +22,76 @@ OutA : 		out std_logic_vector(8 downto 0);
 OutB : 		out std_logic_vector(8 downto 0);
 OutC : 		out std_logic_vector(8 downto 0);
 OutD : 		out std_logic_vector(8 downto 0);
-wport: 		out std_logic_vector(3 downto 0);
+wportCross: 		out std_logic_vector(3 downto 0);
 
 reset : 	in std_logic
 );
 end FCSBlockTop;
 
-architecture of FCSBlockTop is
+architecture arch of FCSBlockTop is
+--Component assignments --
+
+component fcs_check_parallel
+port(
+	clk            : IN std_logic;                      -- system clock
+	reset          : IN std_logic;                      -- asynchronous reset
+	start_of_frame : IN std_logic;                      -- arrival of the first bit.
+	end_of_frame   : IN std_logic;                      -- arrival of the first bit in FCS.
+	data_in        : IN std_logic_vector(7 DOWNTO 0);   -- serial input data.
+	fcs_error      : OUT std_logic                      -- indicates an error.
+	);
+end component;
 --Signal assignments.--
+
+--Component connection vectors.
+signal SoF : std_logic_vector(3 downto 0) := (others => '0');		--Start of Frame
+signal EoF : std_logic_vector(3 downto 0) := (others => '0');		--End of Frame
+signal data_in : std_logic_vector(31 downto 0) := (others => '0');
+signal fcs_error : std_logic_vector(3 downto 0) := (others => '0');
 
 begin
 
+
+
+
+
+--Port Mapping--
+FCS1 : fcs_check_parallel
+port map(
+	clk => clk,
+	reset => reset,
+	start_of_frame => SoF(0),
+	end_of_frame => EoF(0),
+	data_in => data_in(7 downto 0),
+	fcs_error => fcs_error(0)
+	);
+FCS2 : fcs_check_parallel
+port map(
+	clk => clk,
+	reset => reset,
+	start_of_frame => SoF(1),
+	end_of_frame => EoF(1),
+	data_in => data_in(15 downto 8),
+	fcs_error => fcs_error(1)
+	);
+FCS3 : fcs_check_parallel
+port map(
+	clk => clk,
+	reset => reset,
+	start_of_frame => SoF(2),
+	end_of_frame => EoF(2),
+	data_in => data_in(23 downto 16),
+	fcs_error => fcs_error(2)
+	);
+FCS4 : fcs_check_parallel
+port map(
+	clk => clk,
+	reset => reset,
+	start_of_frame => SoF(3),
+	end_of_frame => EoF(3),
+	data_in => data_in(31 downto 24),
+	fcs_error => fcs_error(3)
+	);
 
 end architecture;
 
